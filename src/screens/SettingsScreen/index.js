289361273f4fbb6d58c3eb1a4a7f6649dba47215
Jookare <134264039+Jookare@users.noreply.github.com>
2024-12-23 23:15:48 +0200

    Update version number

--- src/screens/SettingsScreen/index.js
+++ src/screens/SettingsScreen/index.js
@@ -146,7 +146,7 @@ const SettingsScreen = () => {
 				</View>
 
 			</View>
-			<Text style={styles.heading}>App version V.1.1.7</Text>
+			<Text style={styles.heading}>App version V.1.1.8</Text>
 		</View>
 	)
 }
